526627e084a27e9695ef18f89a78e5b88b0e3802
syedali <infosyedalimoiz99@gmail.com>
2024-10-30 21:25:58 +0500

    live setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import './App.css';
-import { AdminNavbar, AdminFooter } from './components/Admin/Index';
-import { AuthorNavbar, AuthorFooter } from './components/Author/Index';
+// import { AdminNavbar, AdminFooter } from './components/Admin/Index';
+// import { AuthorNavbar, AuthorFooter } from './components/Author/Index';
 import { Layout, Blogs, ViewBlog, Home } from './components/Home/Index';
 
 const App = () => {
   return (
     <>
-      <Layout/>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='blogs' element={<Blogs />} />
+          <Route path='blog' element={<ViewBlog />} />
+        </Route>
+      </Routes>
     </>
   );
 };
--- src/components/Author/Footer.jsx
+++ src/components/Author/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
@@ -6,19 +7,19 @@ export default function Footer() {
     <footer className="footer" role="contentinfo" itemScope itemType="http://schema.org/WPFooter">
     <div className="social" role="navigation" aria-labelledby="social-heading">
         <h3 id="social-heading" className="sr-only">Follow us on social media</h3>
-        <a href="#" aria-label="Facebook"><i className="fa-brands fa-facebook" /></a>
-        <a href="#" aria-label="Twitter"><i className="fa-brands fa-x-twitter" /></a>
-        <a href="#" aria-label="Mastodon"><i className="fa-brands fa-mastodon" /></a>
-        <a href="#" aria-label="Instagram"><i className="fa-brands fa-instagram" /></a>
+        <Link aria-label="Facebook"><i className="fa-brands fa-facebook" /></Link>
+        <Link aria-label="Twitter"><i className="fa-brands fa-x-twitter" /></Link>
+        <Link aria-label="Mastodon"><i className="fa-brands fa-mastodon" /></Link>
+        <Link aria-label="Instagram"><i className="fa-brands fa-instagram" /></Link>
     </div>
     <hr className="footer-break" />
     <ul className="footer-links" role="navigation" aria-labelledby="footer-links-heading">
         <h3 id="footer-links-heading" className="sr-only">Footer Links</h3>
-        <li><a href="#">Site Home</a></li>
-        <li><a href="#">Playground</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Sitemap</a></li>
-        <li><a href="#">Contents</a></li>
+        <li><Link>Site Home</Link></li>
+        <li><Link>Playground</Link></li>
+        <li><Link>About</Link></li>
+        <li><Link>Sitemap</Link></li>
+        <li><Link>Contents</Link></li>
     </ul>
     <p className="copyright">© 2024 SDavidPrince. Demo of a footer. Some Rights Reserved</p>
     </footer>
