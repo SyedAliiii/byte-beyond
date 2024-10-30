a35ffb0c88ea4c9635266cca74e7837cee39a9b0
syedali <infosyedalimoiz99@gmail.com>
2024-10-30 21:20:15 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AdminNavbar, AdminFooter } from './components/Admin/Index';
 import { AuthorNavbar, AuthorFooter } from './components/Author/Index';
 import { Layout, Blogs, ViewBlog, Home } from './components/Home/Index';
+
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='/blogs' element={<Blogs/>}/>
-        <Route path='/blog' element={<ViewBlog/>}/>
-      </Route>
-    </Routes>
+      <Layout/>
     </>
   );
 };
+
 export default App;
